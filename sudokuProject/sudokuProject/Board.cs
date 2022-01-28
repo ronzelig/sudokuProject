91475431395f12891a61f13df812c6526cb7196b
ronzelig <zelig.ron2003@gmail.com>
2022-01-28 16:30:45 +0200

    create and print board

---
+++ sudokuProject/sudokuProject/Board.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sudokuProject
+{
+    class Board
+    {
+        private Cell[,] board;
+        private int dimensionSize;
+        //private HashSet<char>[] rowsValues;
+        //private HashSet<char>[] colsValues;
+        //private HashSet<char>[] squaresValues;
+
+        public Board(string inputString)
+        {
+            dimensionSize = (int)Math.Sqrt(inputString.Length);
+            board = new Cell[dimensionSize, dimensionSize];
+            char temp;
+            for (int row = 0; row < dimensionSize; row++)
+            {
+                for (int col = 0; col < dimensionSize; col++)
+                {
+                    temp = inputString[row * dimensionSize + col];
+                    board[row, col] = new Cell(row, col, temp);
+                }
+            }
+        }
+
+        public void printBoard()
+        {
+            for (int row = 0; row < dimensionSize; row++)
+            {
+                for (int col = 0; col < dimensionSize; col++)
+                {
+                    Console.Write(board[row, col]);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ sudokuProject/sudokuProject/Cell.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sudokuProject
+{
+    class Cell
+    {
+        public int row { get;}
+        public int col { get; }
+        public int square { get; }
+        public int[] options { get; set; }
+        public char value { get; set; }
+
+        public Cell(int row, int col, char value)
+        {
+            this.row = row;
+            this.col = col;
+            this.value = value;
+            //this.square = indexToSquare(row, col);
+        }
+
+        private bool isEmpty()
+        {
+            return this.value == '0';
+        }
+
+        public override string ToString()
+        {
+            return char.ToString(value);
+        }
+    }
+}
